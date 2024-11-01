ea68035684032351576d4b4695e0e0c63a6dca5a
himansu naik <naikhimansu546@gmail.com>
2024-11-01 18:35:08 +0530

    resolve api url

--- Profile-Store-Mini-Social-Media/frontend/src/utils/utils.js
+++ Profile-Store-Mini-Social-Media/frontend/src/utils/utils.js
@@ -1,2 +1,3 @@
 // export const apiUrl = process.env.REACT_APP_API_URL_LOCAL;
-export const apiUrl = https://profile-store-mini-social-media.onrender.com;
+// export const apiUrl = "http://localhost:8000";
+export const apiUrl = "https://profile-store-mini-social-media.onrender.com";
