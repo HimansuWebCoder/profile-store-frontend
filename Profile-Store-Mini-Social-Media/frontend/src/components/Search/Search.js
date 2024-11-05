1b325259a47d22e16cb7152e67bf8311115c8810
himansu naik <naikhimansu546@gmail.com>
2024-11-05 15:52:02 +0530

    updated and resolved issue

--- Profile-Store-Mini-Social-Media/frontend/src/components/Search/Search.js
+++ Profile-Store-Mini-Social-Media/frontend/src/components/Search/Search.js
@@ -1,15 +1,18 @@
 import "./Search.css";
-
 function Search() {
 	return (
-		// <div className="search-container">
 		<input
-			// className="w-full h-10 max-w-[300px] border-none rounded-full outline-none block bg-[url('../../../public/assets/images/search.png')] bg-no-repeat bg-[10px_center] bg-[20px_20px] box-border text-xl pl-[40px] placeholder:text-[rgba(255,255,255,0.9)] placeholder:italic placeholder:shadow-md sm:w-[30%] sm:text-sm"
-			id="search-input"
-			type="text"
+			className="w-full h-10 max-w-[300px] border-none rounded-full outline-none block bg-no-repeat box-border text-xl pl-[40px] placeholder:text-black placeholder:italic  sm:w-[30%] sm:text-sm"
 			placeholder="Search Profiles"
+			type="text"
+			style={{
+				backgroundImage: "url(/assets/images/search.png)",
+				backgroundPosition: "10px center",
+				backgroundSize: "20px 20px",
+				backgroundRepeat: "no-repeat",
+				color: "white",
+			}}
 		/>
-		// </div>
 	);
 }
 
