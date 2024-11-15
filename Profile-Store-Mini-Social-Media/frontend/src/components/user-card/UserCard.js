fda23c17e3148585b802f5083df1d01d8985983c
himansu naik <naikhimansu546@gmail.com>
2024-11-15 15:49:34 +0530

    add loader in profile card fetch

--- Profile-Store-Mini-Social-Media/frontend/src/components/user-card/UserCard.js
+++ Profile-Store-Mini-Social-Media/frontend/src/components/user-card/UserCard.js
@@ -1,11 +1,13 @@
 import { useState,useEffect } from "react";
 import { Link, useLocation, useParams } from "react-router-dom";
 import { apiUrl } from "../../utils/utils";
+import CircularProgress from "@mui/material/CircularProgress";
 import "./UserCard.css";
 
 function UserCard() {
 	const [ userCardInfo, setUserCardInfo ] = useState("");
 	const [ users, setUsers ] = useState({});
+	const [ loading, setLoading ] = useState(true);
 	const location = useLocation();
 	const userId = location.pathname.split("/")[2]
 
@@ -22,6 +24,7 @@ function UserCard() {
 		.then(res => res.json())
 		.then(user => {
 			setUsers(user)
+			setLoading(false)
 			// console.log(user)
 			// if (user.length > 0) {
 			// setUsers(user);
@@ -33,18 +36,22 @@ function UserCard() {
 	}, [])
 
 	return (
-		<div className="usercard-container">
-			<img src={users?.profilePhoto?.[0]?.image} alt="profile image" className="rounded-full aspect-square" />
-			<h1 className="text-[2rem] bg-black">{users?.user?.[0]?.name}</h1>
-			<h1 className="text-[1.5rem] bg-black mt-[5px]">{users?.user?.[0]?.headline}</h1>
-			<h1 className="text-[1.2rem] bg-blue-500">{users?.about?.[0]?.description}</h1>
-			{/*<p>Skills: {users?.skill?.[1]?.skill || "No skills available"}</p>*/}
-			<p className="text-[1.2rem] bg-green-500 mt-[10px]">
-			  Skills: {users?.skill?.length > 0 ? users.skill.map((item, index) => (
-			    <span key={index}>{item.skill}{index < users.skill.length - 1 && ', '}</span>
-			  )) : "No skills available"}
-			</p>
-		</div>
+	       
+	       	loading ? <CircularProgress /> : (
+			<div className="max-w-[500px] h-auto p-2 mx-auto text-white mt-5 flex justify-start items-center flex-col text-center">
+				<img src={users?.profilePhoto?.[0]?.image} alt="profile image" className="rounded-full aspect-square" />
+				<h1 className="text-[2rem] bg-black">{users?.user?.[0]?.name}</h1>
+				<h1 className="text-[1.5rem] bg-black mt-[5px]">{users?.user?.[0]?.headline}</h1>
+				<h1 className="text-[1.2rem] bg-blue-500">{users?.about?.[0]?.description}</h1>
+				{/*<p>Skills: {users?.skill?.[1]?.skill || "No skills available"}</p>*/}
+				<p className="text-[1.2rem] bg-green-500 mt-[10px]">
+				  Skills: {users?.skill?.length > 0 ? users.skill.map((item, index) => (
+				    <span key={index}>{item.skill}{index < users.skill.length - 1 && ', '}</span>
+				  )) : "No skills available"}
+				</p>
+			</div>
+	       		)
+	       
 	)
 }
 
