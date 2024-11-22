8aab56f4509d920e2c3b053c3c768a89ae1725cd
himansu naik <naikhimansu546@gmail.com>
2024-11-22 18:59:11 +0530

    update menu in welcome home page

--- Profile-Store-Mini-Social-Media/frontend/src/containers/WelcomePage/Welcome.js
+++ Profile-Store-Mini-Social-Media/frontend/src/containers/WelcomePage/Welcome.js
@@ -1,10 +1,52 @@
+import {useState} from 'react';
 import Logo from "../../components/Logo/Logo"
 import Signup from "../../components/Auth/Signup/Signup"
 import Login from "../../components/Auth/Login/Login"
 import { Link } from "react-router-dom";
 import "./Welcome.css";
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+// import InboxIcon from '@mui/icons-material/MoveToInbox';
+// import MailIcon from '@mui/icons-material/Mail';
 
 function Welcome() {
+
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+   const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <div className="text-white flex justify-center flex-col  w-[200px] ">
+                	<button className="bg-[#31363F] max-w-[auto]  h-auto p-1 rounded">About</button>
+                	<button className="bg-[#31363F] max-w-[auto] mt-1  h-auto p-1 rounded">Explore</button>
+                	<button className="bg-[#31363F] max-w-[auto] mt-1  h-auto p-1 rounded">Signup</button>
+                	<button className="bg-[#31363F] max-w-[auto] mt-1 p-1 rounded"><Link to="/home">Login</Link></button>
+                </div>
+              </ListItemIcon>
+              <ListItemText />
+            </ListItemButton>
+          </ListItem>
+      
+      </List>
+    </Box>
+  );
+
+
 	return (
      <div className="max-w-[100%] h-auto p-1 text-white bg-[#1E201E] ">
      	<nav className="flex justify-between items-center bg-[#1E201E] border-b border-1 border-gray-500 h-auto w-[100%]">
@@ -12,7 +54,7 @@ function Welcome() {
      		{/*<Logo />*/}
      	    <img src="/assets/images/Profile_Logo.png" className="max-w-[80px] h-auto m-2" />
      	</div>
-     		<div className="w-[100%]">
+     		{/*<div className="w-[100%]">
      			<ul className="flex justify-center flex-wrap items-center w-[100%]">
      				<li className="mr-5">About</li>
      				<li className="mr-5">Explore</li>
@@ -24,7 +66,18 @@ function Welcome() {
      				</li>
      			</ul>
 
-     		</div>
+     		</div>*/}
+
+     		<Button sx={{color: "white"}} onClick={toggleDrawer(true)}>Menu</Button>
+
+     		<Drawer 
+     		sx={{
+			    '& .MuiDrawer-paper': {
+			      backgroundColor: 'lightblue', // Full background color
+			    },
+			  }} open={open} onClose={toggleDrawer(false)}>
+	        {DrawerList}
+	      </Drawer>
      	</nav>
 	     			{/*<div className="text-center text-[2rem] bg-violet-500 mt-2">
 	     				<h1>Welcome To Profile-Store</h1>
