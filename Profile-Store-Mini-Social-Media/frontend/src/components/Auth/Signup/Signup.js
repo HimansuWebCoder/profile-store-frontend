690829ca0d31c8e68156f0064d0332f7579bb571
himansu naik <naikhimansu546@gmail.com>
2024-11-22 16:07:08 +0530

    updated style of form font and form

--- Profile-Store-Mini-Social-Media/frontend/src/components/Auth/Signup/Signup.js
+++ Profile-Store-Mini-Social-Media/frontend/src/components/Auth/Signup/Signup.js
@@ -48,17 +48,17 @@ function submitHandler() {
 	return (
 		<div className="max-w-[500px] m-auto mt-5 p-5 h-auto bg-[#222831] rounded-[5px] flex justify-center flex-col text-center ">
 		    <div className="w-full  h-5 flex justify-end">
-		    	<div className="w-[30px] h-[30px] rounded-full border"></div>
+		    	<div className="w-[20px] h-[20px] rounded-full border"></div>
 		    </div>
 		    <h1 className="text-white font-bold text-[2rem]">Signup</h1>
-		    <h2>Name</h2>
+		    <h2 className="text-[1.5rem]">Name</h2>
 			    <input className="text-black w-[80%] m-auto p-2 rounded text-[1.2rem]" type="text" value={name} onChange={nameHandler} placeholder="Enter your Name " /><br/>
-		    <h2>Email</h2>
+		    <h2 className="text-[1.5rem]">Email</h2>
 	            <input className="text-black w-[80%] m-auto p-2 rounded text-[1.2rem]" type="text" value={signupInput} onChange={inputHandler} placeholder="Enter Your Email" /><br/>
-		    <h2>Password</h2>
+		    <h2 className="text-[1.5rem]">Password</h2>
 	            <input className="text-black w-[80%] m-auto p-2 rounded text-[1.2rem]" type="text" value={password} onChange={passwordHandler} placeholder="Enter your Password "/><br/>
 	            <div className="w-full   h-auto flex justify-between">
-		    	<div className="w-[30px] h-[30px] rounded-full border"></div>
+		    	<div className="w-[20px] h-[20px] rounded-full border"></div>
 	            <button className="text-white text-[1.2rem] bg-[#31363F] w-auto p-2 rounded  mr-11" onClick={submitHandler}>submit</button>
 		    </div>
 		</div>
